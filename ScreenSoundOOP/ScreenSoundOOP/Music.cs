16a701cce630eebe87c1d966d28b58259804bbeb
Gardenia Oliveira <jobs.gardeniaoliveira@gmail.com>
2024-09-09 16:32:14 +0100

    update screensound project with oop

---
+++ ScreenSoundOOP/ScreenSoundOOP/Music.cs
@@ -0,0 +1,37 @@
+﻿class Music
+{
+    //propriets = when have get and set
+    public string Title { get; set; }
+    public string Artist { get; set; }
+    public  int Duration { get; set; }
+    public bool Available { get; set; }
+
+    //i only what read, so i use only get
+    public string Description {
+        get
+        {
+            return $"The music {Title} belong to the band {Artist}";
+        }
+    }
+
+    //another way to write the code 
+    //public string Description => $"The music {Title} belong to the band {Artist}";
+
+
+    //method
+    public void ShowMusicInfo()
+    {
+        Console.WriteLine($"Title : {Title}");
+        Console.WriteLine($"Artist : {Artist}");
+        Console.WriteLine($"Duration : {Duration}");
+     
+        if (Available)
+        {
+            Console.WriteLine("It is available in your plan");
+        }
+        else
+        {
+            Console.WriteLine("Upgrade your plan to plus+");
+        }
+    }
+}
---
+++ ScreenSoundOOP/ScreenSoundOOP/Program.cs
@@ -0,0 +1,18 @@
+﻿Music music1 = new Music();
+music1.Title = "Roxanne";
+music1.Artist = "The police";
+music1.Duration = 60;
+music1.Available =true;
+
+Console.WriteLine(music1.Description);
+music1.ShowMusicInfo();
+
+Console.WriteLine("***************************");
+Music music2 = new Music();
+music2.Title = "A lua me traiu";
+music2.Artist = "Calypso";
+music2.Duration = 120;
+music2.Available = false;
+
+Console.WriteLine(music2.Description);
+music2.ShowMusicInfo();
